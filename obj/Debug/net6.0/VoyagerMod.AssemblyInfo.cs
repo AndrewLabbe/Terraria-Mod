265e3ba0f6e20f1e42df42f3afb1249eb5371c2c
AndrewLabbe <ablabbe@quinnipiac.edu>
2024-02-28 21:09:44 -0500

    Inital World Generation changes, malachite ore now is generated freely

--- obj/Debug/net6.0/VoyagerMod.AssemblyInfo.cs
+++ obj/Debug/net6.0/VoyagerMod.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("VoyagerMod")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0f3d8a08010ea74de61206900383b44a4d447dea")]
 [assembly: System.Reflection.AssemblyProductAttribute("VoyagerMod")]
 [assembly: System.Reflection.AssemblyTitleAttribute("VoyagerMod")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
