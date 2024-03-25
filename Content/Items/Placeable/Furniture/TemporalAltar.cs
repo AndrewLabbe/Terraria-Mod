a1736f51643e6cdaa019d494e36db4192899d3a2
Garrett Goldberg <98359240+gegoldberg@users.noreply.github.com>
2024-03-24 21:27:58 -0400

    Created Temporal Altar Crafting Station

---
+++ Content/Items/Placeable/Furniture/TemporalAltar.cs
@@ -0,0 +1,40 @@
+using VoyagerMod.Content.Items;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace VoyagerMod.Content.Items.Placeable.Furniture
+{
+    [LegacyName("TemporalBasin")]
+    public class TemporalAltar : ModItem, ILocalizedModType
+    {
+        public new string LocalizationCategory => "Content.Placeables";
+        public override void SetDefaults()
+        {
+            Item.width = 8;
+            Item.height = 10;
+            Item.maxStack = 9999;
+            Item.useTurn = true;
+            Item.autoReuse = true;
+            Item.useAnimation = 15;
+            Item.useTime = 10;
+            Item.useStyle = ItemUseStyleID.Swing;
+            Item.consumable = true;
+            Item.createTile = ModContent.TileType<Content.Tiles.TemporalAltar>();
+        }
+
+		public override void ModifyResearchSorting(ref ContentSamples.CreativeHelper.ItemGroup itemGroup)
+		{
+			itemGroup = ContentSamples.CreativeHelper.ItemGroup.CraftingObjects;
+		}
+
+        public override void AddRecipes()
+        {
+            CreateRecipe().
+                AddIngredient<AstroniumBar>(10).
+                AddIngredient<MalachiteBar>(5).
+                AddIngredient<FluoriteBar>(5).
+                AddTile(TileID.LunarCraftingStation).
+                Register();
+        }
+    }
+}
---
+++ Content/Tiles/TemporalAltar.cs
@@ -0,0 +1,53 @@
+using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.DataStructures;
+using Terraria.ID;
+using Terraria.Localization;
+using Terraria.ModLoader;
+using Terraria.ObjectData;
+
+namespace VoyagerMod.Content.Tiles
+{
+    [LegacyName("TemporalBasin")]
+    public class TemporalAltar : ModTile
+    {
+        public override void SetStaticDefaults()
+        {
+            Main.tileLighted[Type] = true;
+            Main.tileFrameImportant[Type] = true;
+            Main.tileLavaDeath[Type] = false;
+            TileObjectData.newTile.CopyFrom(TileObjectData.Style3x3);
+            TileObjectData.newTile.LavaDeath = false;
+            TileObjectData.newTile.StyleLineSkip = 2;
+            TileObjectData.addTile(Type);
+            AnimationFrameHeight = 54;
+            AddToArray(ref TileID.Sets.RoomNeeds.CountsAsTorch);
+        }
+
+        public override void AnimateTile(ref int frame, ref int frameCounter)
+        {
+            frameCounter++;
+            if (frameCounter >= 6)
+            {
+                frame = (frame + 1) % 9;
+                frameCounter = 0;
+            }
+        }
+
+        public override void ModifyLight(int i, int j, ref float r, ref float g, ref float b)
+        {
+            if (Main.tile[i, j].TileFrameX < 18)
+            {
+                r = 1f;
+                g = 0.85f;
+                b = 0.7f;
+            }
+            else
+            {
+                r = 0f;
+                g = 0f;
+                b = 0f;
+            }
+        }
+    }
+}
