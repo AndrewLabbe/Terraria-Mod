3a162aaeb3b5808fbd161d90ed107e5b026cc9f9
Garrett Goldberg <98359240+gegoldberg@users.noreply.github.com>
2024-03-25 01:18:09 -0400

    Added and Updated Crafting Recipe for Fluorite Trident

--- Content/Items/FluoriteTrident.cs
+++ Content/Items/FluoriteTrident.cs
@@ -2,6 +2,7 @@ using VoyagerMod.Content.Projectiles;
 using Terraria;
 using Terraria.ID;
 using Terraria.ModLoader;
+using VoyagerMod.Content.Items.Placeable;
 
 namespace VoyagerMod.Content.Items
 {
@@ -33,6 +34,14 @@ namespace VoyagerMod.Content.Items
             Item.shoot = ModContent.ProjectileType<FluoriteTridentProj>();
             Item.shootSpeed = 6f;
         }
+
+        public override void AddRecipes()
+		{
+			Recipe recipe = CreateRecipe();
+			recipe.AddIngredient<FluoriteBar>(14);
+			recipe.AddTile(TileID.Anvils);
+			recipe.Register();
+		}
 
         public override bool CanUseItem(Player player) => player.ownedProjectileCounts[Item.shoot] <= 0;
     }
