168e7f2044ffc0cc65b6633d365f6ae5d71e530d
Garrett Goldberg <98359240+gegoldberg@users.noreply.github.com>
2024-03-31 22:18:02 -0400

    Created Astronium Bow

---
+++ Content/Items/AstroniumBow.cs
@@ -0,0 +1,45 @@
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using VoyagerMod.Content.Items.Placeable;
+
+namespace VoyagerMod.Content.Items
+{
+    public class AstroniumBow : ModItem, ILocalizedModType
+    {
+        public new string LocalizationCategory => "Items.Weapons.Ranged";
+        public override void SetDefaults()
+        {
+            Item.damage = 92;
+            Item.DamageType = DamageClass.Ranged;
+            Item.width = 38;
+            Item.height = 78;
+            Item.useTime = 6;
+            Item.useAnimation = 18;
+            Item.reuseDelay = 18;
+            Item.useLimitPerAnimation = 3;
+            Item.useStyle = ItemUseStyleID.Shoot;
+            Item.noMelee = true;
+            Item.knockBack = 2.5f;
+            Item.value = 90;
+            Item.rare = ItemRarityID.Cyan;
+            Item.UseSound = SoundID.Item5;
+            Item.autoReuse = true;
+            Item.shoot = ProjectileID.WoodenArrowFriendly;
+            Item.shootSpeed = 16f;
+            Item.useAmmo = AmmoID.Arrow;
+            // Item.Voyager().canFirePointBlankShots = true;
+        }
+
+        // Terraria seems to really dislike high crit values in SetDefaults
+        public override void ModifyWeaponCrit(Player player, ref float crit) => crit += 25;
+
+        public override void AddRecipes()
+        {
+            CreateRecipe().
+                AddIngredient<AstroniumBar>(7).
+                AddTile(TileID.Anvils).
+                Register();
+        }
+    }
+}
