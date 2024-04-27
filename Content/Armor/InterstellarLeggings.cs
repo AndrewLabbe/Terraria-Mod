2512514297841a3a4297dcc759dfff41f1e6f2eb
AndrewLabbe <ablabbe@quinnipiac.edu>
2024-04-27 18:35:37 -0400

    Updated stats for Interstellar armor

--- Content/Armor/InterstellarLeggings.cs
+++ Content/Armor/InterstellarLeggings.cs
@@ -1,6 +1,7 @@
 using Terraria;
 using Terraria.Enums;
 using Terraria.ID;
+using Terraria.Localization;
 using Terraria.ModLoader;
 using VoyagerMod.Content.Armor;
 using VoyagerMod.Content.Items.Placeable;
@@ -11,21 +12,25 @@ namespace VoyagerMod.Content.Armor
     public class InterstellarLeggings : ModItem, ILocalizedModType
     {
         public new string LocalizationCategory => "Items.Armor.PostMoonLord";
+        public static int MoveSpeedBonus = 30;
+        public static readonly int AdditiveGenericDamageBonus = 30;
+        public override LocalizedText Tooltip => base.Tooltip.WithFormatArgs(AdditiveGenericDamageBonus, MoveSpeedBonus);
+
         public override void SetDefaults()
         {
             Item.width = 18;
             Item.height = 18;
             Item.value = 15;
-            Item.defense = 44;
-            Item.rare = ItemRarityID.LightPurple;
+            Item.defense = 38;
+            Item.rare = ItemRarityID.Purple;
         }
 
         public override void UpdateEquip(Player player)
         {
-            player.moveSpeed += 0.1f;
             player.carpet = true;
-            player.GetDamage<GenericDamageClass>() += 0.12f;
-            player.GetCritChance<GenericDamageClass>() += 5;
+            player.GetDamage<GenericDamageClass>() += 0.15f;
+            player.moveSpeed += MoveSpeedBonus / 100f;
+            player.waterWalk = true;
         }
 
         public override void AddRecipes()
